441cf1aa3ef9f03ee8e5d7e4108b23b7ef62b7b0
Kolchy <kolchy@users.noreply.github.com>
2016-08-05 10:50:23 +1000

    Adding support for IE11.
    Based on conversations from here: http://stackoverflow.com/a/37766736/94157

--- angular-js-xlsx.js
+++ angular-js-xlsx.js
@@ -1,49 +1,74 @@
 'use strict';
 
 angular.module('angular-js-xlsx', [])
-.directive('jsXlsx', function () {
-  return {
-    restrict: 'E',
-    template: '<input type="file" />',
-    replace: true,
-    link: function (scope, element, attrs) {
+  .directive('jsXls', function () {
+    return {
+      restrict: 'E',
+      template: '<input type="file" />',
+      replace: true,
+      link: function (scope, element, attrs) {
 
-      function handleSelect() {
-        var files = this.files;
-        for (var i = 0, f = files[i]; i != files.length; ++i) {
-          var reader = new FileReader();
-          var name = f.name;
-          reader.onload = function(e) {
-            var data = e.target.result;
+        function handleSelect() {
+          var files = this.files;
+          for (var i = 0, f = files[i]; i != files.length; ++i) {
+            var reader = new FileReader();
+            var name = f.name;
+            reader.onload = function (e) {
+              if (!e) {
+                var data = reader.content;
+              } else {
+                var data = e.target.result;
+              }
 
-            /* if binary string, read with type 'binary' */
-            try {
-              var workbook = XLS.read(data, {type: 'binary'});
+              /* if binary string, read with type 'binary' */
+              try {
+                var workbook = XLS.read(data, { type: 'binary' });
 
-              if (attrs.onread) {
-                var handleRead = scope[attrs.onread];
-                if (typeof handleRead === "function") {
-                  handleRead(workbook);
+                if (attrs.onread) {
+                  var handleRead = scope[attrs.onread];
+                  if (typeof handleRead === "function") {
+                    handleRead(workbook);
+                  }
+                }
+              } catch (e) {
+                if (attrs.onerror) {
+                  var handleError = scope[attrs.onerror];
+                  if (typeof handleError === "function") {
+                    handleError(e);
+                  }
                 }
               }
-            } catch(e) {
-              if (attrs.onerror) {
-                var handleError = scope[attrs.onerror];
-                if (typeof handleError === "function") {
-                  handleError(e);
+
+              // Clear input file
+              element.val('');
+            };
+
+            //extend FileReader
+            if (!FileReader.prototype.readAsBinaryString) {
+              FileReader.prototype.readAsBinaryString = function (fileData) {
+                var binary = "";
+                var pt = this;
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                  var bytes = new Uint8Array(reader.result);
+                  var length = bytes.byteLength;
+                  for (var i = 0; i < length; i++) {
+                    binary += String.fromCharCode(bytes[i]);
+                  }
+                  //pt.result  - readonly so assign binary
+                  pt.content = binary;
+                  $(pt).trigger('onload');
                 }
+                reader.readAsArrayBuffer(fileData);
               }
             }
 
-            // Clear input file
-            element.val('');
-          };
+            reader.readAsBinaryString(f);
 
-          reader.readAsText(f);
+          }
         }
-      }
 
-      element.on('change', handleSelect);
-    }
-  };
-});
+        element.on('change', handleSelect);
+      }
+    };
+  });
